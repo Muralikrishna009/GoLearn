64166e9e8febdb1b74d5261588a5ec86ff42b8cf
Muralikrishna009 <muralikrishna95535@gmail.com>
2024-03-17 12:42:19 +0530

    data types

--- basics/main.go
+++ basics/main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Hello world!")
+
+	fmt.Println("------------")
+
+	fmt.Println("Data Types!")
+
+	var myVar string = "first one"
+
+	fmt.Println(myVar)
+	myVar += "new sting added to empty"
+
+	fmt.Println(myVar)
+
+	var firstInt int
+	fmt.Println(firstInt)
+	fmt.Println("the above was int declaration without any specification")
+
+	var secondInt int8 = 127
+	fmt.Println(secondInt)
+
+	fmt.Println("add 1 to int8 number which has the current value of 127")
+
 }
