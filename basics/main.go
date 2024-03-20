c15cfad6d6cec4ef8e54dd3b2c3f960a58dbd82c
Muralikrishna009 <muralikrishna95535@gmail.com>
2024-03-20 11:35:07 +0530

    go

--- basics/main.go
+++ basics/main.go
@@ -2,27 +2,41 @@ package main
 
 import "fmt"
 
+// import "unicode/utf8"
+
 func main() {
-	fmt.Println("Hello world!")
+	//printMe()
 
-	fmt.Println("------------")
+	 myArr := [3]int32{1, 2, 3}
 
-	fmt.Println("Data Types!")
+	fmt.Println(myArr)
 
-	var myVar string = "first one"
-
-	fmt.Println(myVar)
-	myVar += "new sting added to empty"
-
-	fmt.Println(myVar)
-
-	var firstInt int
-	fmt.Println(firstInt)
-	fmt.Println("the above was int declaration without any specification")
-
-	var secondInt int8 = 127
-	fmt.Println(secondInt)
-
-	fmt.Println("add 1 to int8 number which has the current value of 127")
+	fmt.Println(len(myArr))
 
 }
+
+// func printMe(){
+// 	fmt.Println("Hello world!")
+
+// 	fmt.Println("------------")
+
+// 	fmt.Println("Data Types!")
+
+// 	var myVar string = "first one"
+
+// 	fmt.Println(myVar)
+// 	myVar += "new sting added to empty"
+
+// 	fmt.Println(myVar)
+
+// 	var firstInt int
+// 	fmt.Println(firstInt)
+// 	fmt.Println("the above was int declaration without any specification")
+
+// 	var secondInt int8 = 127
+// 	fmt.Println(secondInt)
+
+// 	fmt.Println("add 1 to int8 number which has the current value of 127")
+
+// 	fmt.Println(utf8.RuneCountInString("this is "))
+// }
